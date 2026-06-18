89a7259fa33d8789cc74d8d830a7d5c9d5476fe4
Eduardo Guedes dos Santos Cassin <eduardojuniorlopescassin@gmail.com>
2026-06-18 13:26:59 -0300

    Refactor IMC calculation and add classification output

--- src/imc.js
+++ src/imc.js
@@ -1,6 +1,20 @@
-let peso1 = 63
-let altura1 = 1.60
+let peso = 63
+let altura = 1.60
 
-let IMC1 = peso1 / (altura1 * altura1)
+let imc = peso / (altura * altura)
 
-console.log(`Seu IMC é: ${IMC1.toFixed(2)}`)
+console.log(`Seu IMC é: ${imc.toFixed(2)}`)
+
+if (imc < 18.5) {
+    console.log("Classificação: Abaixo do peso")
+} else if (imc < 25) {
+    console.log("Classificação: Peso normal")
+} else if (imc < 30) {
+    console.log("Classificação: Sobrepeso")
+} else if (imc < 35) {
+    console.log("Classificação: Obesidade Grau I")
+} else if (imc < 40) {
+    console.log("Classificação: Obesidade Grau II")
+} else {
+    console.log("Classificação: Obesidade Grau III")
+}
